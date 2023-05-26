c3b28b8abaf20e7236d9f956a7a9282a70c1b42e
Your Name <you@example.com>
2023-05-27 06:30:37 +0700

    update something

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -5,5 +5,6 @@ class Bill < ApplicationRecord
 	has_and_belongs_to_many :skirts
 	has_and_belongs_to_many :t_shirts
  validates_presence_of :EmID, :NameCus, :Phonenumber, :Product
+ validates_uniqueness_of :Phonenumber
  validates_length_of :Phonenumber, is: 9, message: "must be exactly 9 digits"
 end
