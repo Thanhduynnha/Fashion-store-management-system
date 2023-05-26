30abdff754552814a452db77286e55cbbc1da3c2
Your Name <you@example.com>
2023-05-26 14:19:46 +0700

    admin

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,4 +1,9 @@
 class Bill < ApplicationRecord
+	
+	has_and_belongs_to_many :dresses
+	has_and_belongs_to_many :pants
+	has_and_belongs_to_many :skirts
+	has_and_belongs_to_many :t_shirts
  validates_presence_of :EmID, :NameCus, :Phonenumber, :Product
  validates_length_of :Phonenumber, is: 9, message: "must be exactly 9 digits"
 end
--- app/models/t_shirt.rb
+++ app/models/t_shirt.rb
@@ -1,4 +1,5 @@
 class TShirt < ApplicationRecord
+	has_and_belongs_to_many :bills
 	validates_presence_of :Fullname,:Description,:Price,:Url
 	validates_uniqueness_of :Fullname, :Url 
 end
